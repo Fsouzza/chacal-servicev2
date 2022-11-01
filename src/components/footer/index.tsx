564ffa22d6d163ed659f2c31dd87b591bb4bafb1
F. Souzza <filipesouzza.adm@gmail.com>
2022-11-01 01:14:26 -0300

    refatoracao do component footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,50 +6,44 @@ import { IoLogoTwitter } from 'react-icons/io5';
 
 export const Footer = () =>{
   const rotas = [{
+    titulo: 'Facebook',
     label: <FaFacebook />,
-    text: 'Facebook',
     to: 'https://www.facebook.com'
   }, {
+    titulo: 'Instagram',
     label: <FiInstagram />,
-    text: 'Instagram',
     to: 'https://www.instagram.com'
   }, {
+    titulo: 'Twitter',
     label: <IoLogoTwitter />,
-    text: 'Twitter',
     to: 'https://twitter.com'
   }];
 
   return(
     <footer className={styles.footer}>
       <div className={styles.footer__content}>
-        <div className={styles.footer__img}>
-          <img className={styles.footer__logo} src={logowhite} />
-          <h3>Chacal Service</h3>
+        <div>
+          <h3 className={styles.footer__titulo}>Chacal Service</h3>
         </div>
-        <div className={styles.footer__permalinks}>
-          <h4>Permalinks</h4>
+        <div>
+          <img className={styles.footer__logo} src={logowhite} />
+        </div>
+        <div>
           <ul className={styles.footer__links}>
             {rotas.map((rota, index) => (
-              <li key={index} className={styles.footer__link}>
-                <a href={rota.to} target='blank' >
+              <li key={index} className={styles.footer__links__link}>
+                <a href={rota.to} title={rota.titulo} target='blank' >
                   {rota.label}
                 </a>
-                {rota.text}
               </li>
             ))}
           </ul>
         </div>
-        <div className={styles.footer__endereco}>
-          <h4>Endereço</h4>
-          <ul className={styles.footer__locais}>
-            <li className={styles.footer__descricao}>Rua Hokkaido, 25 - Parque Dez</li>
-            <li className={styles.footer__descricao}>CEP: 69000-000</li>
-            <li className={styles.footer__descricao}>2 andar, sala C-4</li>
-          </ul>
-        </div>
       </div>
-      <div className={styles.copyright}>
-        <small>Todos os direitos reservados | Copyright &copy; Mr Yeagger</small>
+      <div className={styles.footer__copyright}>
+        <small>Desenvolvido por Mr Yeagger</small>
+        <p>|</p>
+        <small>Copyright &copy; Chacal Service</small>
       </div>
     </footer>
   );
