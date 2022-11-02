297d965a239d7604efd1b1d0ca5573beed0ff37d
F. Souzza <filipesouzza.adm@gmail.com>
2022-11-02 13:54:32 -0300

    s

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppRouter from './routers/Routes';
 import 'normalize.css';
 import './index.css';
-import AppRouter from './routers/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
