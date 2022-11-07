61451c5274a38613fbb777bdd3ef73373dcb500e
F. Souzza <filipesouzza.adm@gmail.com>
2022-11-07 02:48:06 -0300

    inicio theme mode

--- src/components/header/dropdownMenus/dropdownSettings/DropdownSettings.tsx
+++ src/components/header/dropdownMenus/dropdownSettings/DropdownSettings.tsx
@@ -3,11 +3,19 @@ import { AiOutlineSetting } from 'react-icons/ai';
 import { BsMoon, BsSun } from 'react-icons/bs';
 import Switch from 'react-switch';
 import styles2 from '../../header.module.scss';
-import { useEffect, useRef, useState } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
+
+type ContextProps = {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext({} as ContextProps);
 
 export const DropdownSettings = () => {
   const [settingsOpen, setSettingsOpen] = useState(false);
   const dropdownSettings = useRef<HTMLDivElement>(null);
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -23,6 +31,10 @@ export const DropdownSettings = () => {
     setSettingsOpen(!settingsOpen);
   };
 
+  const toggleTheme = () => {
+    setTheme((corr) => (corr === 'light' ? 'dark' : 'light'));
+  };
+
   return(
     <div ref={dropdownSettings}>
       <button 
@@ -32,29 +44,31 @@ export const DropdownSettings = () => {
         <AiOutlineSetting />
       </button>
 
-      <div className={settingsOpen ? `${styles.settingsMenu}` : `${styles.settingsMenu__inativo}`}>
-        <div className={styles.titulo}>
-          <h3>Configurações</h3>
+      <ThemeContext.Provider value={{ theme, toggleTheme}}>
+        <div className={settingsOpen ? `${styles.settingsMenu}` : `${styles.settingsMenu__inativo}`} id={theme}>
+          <div className={styles.titulo}>
+            <h3>Configurações</h3>
+          </div>
+          <ul className={styles.items}>
+            <span>Escolha o modo</span>
+            <li>
+              <BsMoon />
+              <Switch 
+                onChange={toggleTheme}
+                checked={theme === 'dark'}
+                handleDiameter={18}
+                height={8}
+                offColor='#4f4f4f'
+                onColor='#202020'
+                uncheckedIcon={false}
+                checkedIcon={false}
+                width={40}
+              />
+              <BsSun />
+            </li>
+          </ul>
         </div>
-        <ul className={styles.items}>
-          <span>Escolha o modo</span>
-          <li>
-            <BsMoon />
-            <Switch 
-              onChange={() => []}
-              checked={false}
-              handleDiameter={18}
-              height={8}
-              offColor='#4f4f4f'
-              onColor='#202020'
-              uncheckedIcon={false}
-              checkedIcon={false}
-              width={40}
-            />
-            <BsSun />
-          </li>
-        </ul>
-      </div>
+      </ThemeContext.Provider>
     </div>
   );
 };
