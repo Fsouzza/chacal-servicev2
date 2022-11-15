43e36f49a007b60538bd20ef0032b001f353189b
F. Souzza <filipesouzza.adm@gmail.com>
2022-11-15 02:47:07 -0300

    restilizacao page clientes

--- src/pages/clientes/Add/index.tsx
+++ src/pages/clientes/Add/index.tsx
@@ -1,17 +1,30 @@
 import styles from './AddContract.module.scss';
+import { FiDownload } from 'react-icons/fi';
+import { contract } from 'data/contratos';
+import { CSVLink } from 'react-csv';
 
 const AddContract = () => {
+  const headers = [
+    {label: 'Id Contrato', key: 'numero'},
+    {label: 'Cliente', key: 'empresa'},
+    {label: 'CNPJ', key: 'cnpj'},
+    {label: 'Serviço', key: 'servico'},
+    {label: 'Valor Reajustado', key: 'vlrReajustado'},
+    {label: 'Data Contrato', key: 'contrato'},
+    {label: 'Faturamento', key: 'faturamento'},
+    {label: 'Situação', key: 'situacao'},
+  ];
+
   return(
-    <section className={styles.search}>
-      <div>
-        <select>
-          <option>Select 1</option>
-          <option>Item 1</option>
-          <option>Item 2</option>
-          <option>Item 3</option>
-        </select>
-        <input type={'text'}></input>
-        <button>Inserir contrato</button>
+    <section className={styles.addBox}>
+      <div className={styles.buttons}>
+        <CSVLink data={contract} headers={headers} filename={'ControleClientes'} className={styles.buttons__export}>
+          <FiDownload size={22} />
+          Export
+        </CSVLink>
+        <button className={styles.buttons__add}>
+          Add Cliente
+        </button>
       </div>
     </section>
   );
